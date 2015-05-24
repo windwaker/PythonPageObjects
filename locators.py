cfdc7e0b31a14a1c1e6cda48b1ee2ac92e4049be
colmh <colm.harrington@gmail.com>
2015-05-24 19:09:43 +0100

    extra whitespace to split class definitions

--- locators.py
+++ locators.py
@@ -4,6 +4,7 @@ class MainPageLocators(object):
     GO_BUTTON  = (By.CSS_SELECTOR, '#submit')
     SEARCH_FOR = (By.CSS_SELECTOR, '#id-search-field')
 
+
 class SearchResultsPageLocators(object):
     GO_HOME = (By.CSS_SELECTOR, '.python-logo')
     RESULTS = (By.CSS_SELECTOR, '.list-recent-events')  
