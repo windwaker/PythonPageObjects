312bf190c9e4f6e6fb5a0654562996b45f0b7428
colmh <colm.harrington@gmail.com>
2015-08-16 22:37:00 +0100

    var name change

--- element.py
+++ element.py
@@ -6,13 +6,13 @@ class BasePageElement(object):
         driver = obj.driver
         print ("Sending '{0}' value to [{1}]".format(val, self.locator))
         WebDriverWait(driver, 10).until(
-            lambda driver: driver.find_element(*self.locator))
+            lambda browser: browser.find_element(*self.locator))
         driver.find_element(*self.locator).send_keys(val)
 
     def __get__(self, obj, typ=None):
         driver = obj.driver
         print ("Getting a value from [{0}]".format(self.locator))
         WebDriverWait(driver, 10).until(
-            lambda driver: driver.find_element(*self.locator))
+            lambda browser: browser.find_element(*self.locator))
         element = driver.find_element(*self.locator)
         return element.get_attribute("value")
